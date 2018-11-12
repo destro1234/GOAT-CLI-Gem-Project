417f226d440117f09ac0ed232c9a9f9595f5fb0e
Elias Ventura <eliasventura23@gmail.com>
2018-11-12 03:04:42 +0000

    working on cli

--- GOAT_CLI/lib/GOAT_CLI/cli.rb
+++ GOAT_CLI/lib/GOAT_CLI/cli.rb
@@ -1,9 +1,9 @@
 class GOATCLI::CLI
 
   def call
-    puts "The Dream Team was the greatest team of NBA superstars ever formed. Here you can compare players and create your own Dream Team starting 5. What position do you want to compare first? Point Guard, Shooting Guard, Small Forward, Power Forward or Center?"
-    list_pointguards
-    stats
+    puts "The Dream Team was the greatest team of NBA superstars ever formed. Here you can compare players and create your own Dream Team starting 5."
+    positions
+    puts "Enter which two players would you like to compare?"
     #if point_guard
       #list_pointguards
     #elsif shooting_guard
@@ -16,6 +16,26 @@ class GOATCLI::CLI
       #list centers
     #end
   end
+
+  def positions
+    puts " What position do you want to compare first? Point Guard, Shooting Guard, Small Forward, Power Forward or Center?"
+    puts <<-DOC
+      1.list point guards
+      2.list shooting guards
+      3.list small forwards
+      4.list power forwards
+      5.list centers
+    DOC
+  end
+
+  def compare
+    puts <<-DOC
+    1.player_1.stats   2.player_2.stats
+    DOC
+
+  end
+
+
 
   def list_pointguards
     puts <<-DOC
