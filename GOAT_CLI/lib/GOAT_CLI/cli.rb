5d28c3bed17d1a9799e03ddeeb0a6142b30b272b
Elias Ventura <eliasventura23@gmail.com>
2018-12-07 04:45:54 +0000

    working on cli

--- GOAT_CLI/lib/GOAT_CLI/cli.rb
+++ GOAT_CLI/lib/GOAT_CLI/cli.rb
@@ -2,6 +2,9 @@ require 'nokogiri'
 require 'pry'
 class GOATCLI::CLI
 
+  @@team = {:pg => "", :sg => "", :sf => "", :pf => "", :c => ""}
+
+
   def initialize
       make_players
   end
@@ -10,21 +13,28 @@ class GOATCLI::CLI
     puts "The Dream Team was the greatest team of NBA superstars ever formed. Here you can compare players and create your own Dream Team starting 5.\n"
     puts "\n"
 
-    GOATCLI::Player.list_of_players
+    list_players
 
     puts "\nEnter 1-50, the first player you would like to compare?"
     first_player = gets.strip
 
-    GOATCLI::Player.list_of_players
+    list_players
 
     puts "\nEnter 1-50, second player you would like to compare?"
     second_player = gets.strip
 
     puts "\n"
-     GOATCLI::Player.find(first_player).to_s  #|| GOATCLI::Player.find_by_name(first_player).to_s
+     if first_player.to_i.between?(1, 50)
+        GOATCLI::Player.find(first_player).to_s
     puts "\n"
-     GOATCLI::Player.find(second_player).to_s   #|| GOATCLI::Player.find_by_name(second_player).to_s
+
+    if second_player.to_i.between?(1, 50) && first_player != second_player
+     GOATCLI::Player.find(second_player).to_s
     puts "\n"
+    else
+      compare
+    end
+    end
     compare
 
     puts "Are you done creating your dream team?(yes/no)"
@@ -32,8 +42,8 @@ class GOATCLI::CLI
     if input.downcase != "yes"
       call
     end
-    #@players = GOATCLI::Player.all
-    @dreamteam = GOATCLI::DreamTeam.new(GOATCLI::Player.team)
+
+    @dreamteam = GOATCLI::DreamTeam.new(@@team)
     puts "\nThis is your Dream Team!"
     @dreamteam.to_s
   end
@@ -55,11 +65,33 @@ end
     elsif input == "add player"
       puts "Which position would you like to add to your team pg, sg, sf, pf or c?"
       position = gets.strip
-      GOATCLI::Player.add_to_team(position)
+      if position == "pg" || position == "sg" || position == "sf" || position == "pf" || position == "c"
+      add_to_team(position.downcase)
       @dreamteam.to_s
     else
       compare
     end
+    end
+  end
 
+  def list_players
+    puts "50 Greatest Basketball Players of All Time"
+    puts "\n"
+    GOATCLI::Player.all.each do |player|
+      puts "#{player.index}. #{player.name}, #{player.position}"
+    end
+  end
+
+  def add_to_team(position)
+    puts "Enter 1-50, Which player would you like to add to your team?"
+    list_players
+    player = gets.strip
+    puts "\n"
+  if player.to_i.between?(1, 50)
+    puts GOATCLI::Player.find(player).to_s
+    @@team[:"#{position}"] = GOATCLI::Player.find(player).name
+  else
+    add_to_team(position)
+  end
   end
 end
